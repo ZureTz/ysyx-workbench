d644c9e5fad4e08fcf034ded666ba1a34a064b3d
ZureTz <2819058133@qq.com>
2025-11-18 17:28:47 +0800

    feat: add LED, switch, and 7-segment display components with rendering support

---
+++ nvboard/example/vsrc/led.v
@@ -0,0 +1,23 @@
+module led(
+  input clk,
+  input rst,
+  input [4:0] btn,
+  input [7:0] sw,
+  output [15:0] ledr
+);
+  parameter CLK_NUM = 10000000;
+
+  reg [31:0] count;
+  reg [7:0] led;
+
+  always @(posedge clk) begin
+    if (rst) begin led <= 1; count <= 0; end
+    else begin
+      if (count == 0) led <= {led[6:0], led[7]};
+      count <= (count >= CLK_NUM ? 32'b0 : count + 1);
+    end
+  end
+
+  // assign ledr = {led[7:5], led[4:0] ^ btn, sw};
+  assign ledr = {led[7:5], led[4:0] ^ btn, led[7:5], led[4:0] ^ btn};
+endmodule
