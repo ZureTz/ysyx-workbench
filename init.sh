1274404760d3dc31671154ccef14d8b1125337c8
Zure <2819058133@qq.com>
2025-11-18 11:32:59 +0800

    refactor: update init.sh to use zsh and modify .gitignore for clarity
    delete: remove unused main.cpp file
    update: modify example.v to define top module with inputs and outputs
    add: implement main.cc for circuit simulation
    add: create xmake.lua for build configuration

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
 
 # usage: addenv env_name path
 function addenv() {
-  sed -i -e "/^export $1=.*/d" ~/.bashrc
-  echo "export $1=`readlink -f $2`" >> ~/.bashrc
-  echo "By default this script will add environment variables into ~/.bashrc."
-  echo "After that, please run 'source ~/.bashrc' to let these variables take effect."
+  sed -i -e "/^export $1=.*/d" ~/.zshrc
+  echo "export $1=`readlink -f $2`" >> ~/.zshrc
+  echo "By default this script will add environment variables into ~/.zshrc."
+  echo "After that, please run 'source ~/.zshrc' to let these variables take effect."
   echo "If you use shell other than bash, please add these environment variables manually."
 }
 
@@ -18,7 +18,7 @@ function init() {
   fi
 
   while [ ! -d $3 ]; do
-    git clone -b $2 git@github.com:$1.git $3
+    git clone -b $2 https://github.com/$1.git $3
   done
   log="$1 `cd $3 && git log --oneline --no-abbrev-commit -n1`"$'\n'
 
