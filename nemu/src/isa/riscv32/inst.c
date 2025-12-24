788c61fb6a3307c56d43718d7f71d725cde6af68
Zure <2819058133@qq.com>
2025-12-24 20:26:10 +0800

    feat: add instruction types and immediate value decoding for J, B, and R types

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -26,6 +26,9 @@ enum {
   TYPE_I,
   TYPE_U,
   TYPE_S,
+  TYPE_J,
+  TYPE_B,
+  TYPE_R,
   TYPE_N, // none
 };
 
@@ -49,6 +52,16 @@ enum {
   do {                                                                         \
     *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7);                   \
   } while (0)
+#define immJ()                                                                 \
+  do {                                                                         \
+    *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) |        \
+           (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1);                   \
+  } while (0)
+#define immB()                                                                 \
+  do {                                                                         \
+    *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) |          \
+           (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1);                     \
+  } while (0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
                            word_t *imm, int type) {
@@ -68,6 +81,18 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2,
     src1R();
     src2R();
     immS();
+    break;
+  case TYPE_J:
+    immJ();
+    break;
+  case TYPE_B:
+    src1R();
+    src2R();
+    immB();
+    break;
+  case TYPE_R:
+    src1R();
+    src2R();
     break;
   case TYPE_N:
     break;
@@ -91,10 +116,31 @@ static int decode_exec(Decode *s) {
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc, U,
           R(rd) = s->pc + imm);
+  INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw, I,
+          R(rd) = Mr(src1 + imm, 4));
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I,
+          R(rd) = src1 + imm);
+  INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu, I,
+          R(rd) = (src1 < imm) ? 1 : 0);
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add, R,
+          R(rd) = src1 + src2);
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub, R,
+          R(rd) = src1 - src2);
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu, I,
           R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb, S,
           Mw(src1 + imm, 1, src2));
+  INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw, S,
+          Mw(src1 + imm, 4, src2));
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq, B,
+          if (src1 == src2) s->dnpc = s->pc + imm);
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bne, B,
+          if (src1 != src2) s->dnpc = s->pc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, R(rd) = s->pc + 4;
+          s->dnpc = s->pc + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr, I,
+          word_t temp = s->pc + 4;
+          s->dnpc = (src1 + imm) & ~1; R(rd) = temp);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N,
           NEMUTRAP(s->pc, R(10))); // R(10) is $a0
