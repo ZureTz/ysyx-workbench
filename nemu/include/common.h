853d0230f82e0245da1ad52e95cb57e82f3ae442
Zure <2819058133@qq.com>
2025-12-24 16:23:04 +0800

    Revert "refactor: consolidate debug and utility functions into common.h, remove unused headers"
    This reverts commit 0f4eb5130840abefa8b3b45897acfe2769ddc194.

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,24 +1,24 @@
 /***************************************************************************************
- * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
- *
- * NEMU is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan
- * PSL v2. You may obtain a copy of Mulan PSL v2 at:
- *          http://license.coscl.org.cn/MulanPSL2
- *
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
- * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
- * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- *
- * See the Mulan PSL v2 for more details.
- ***************************************************************************************/
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
 
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#include <stdint.h>
 #include <inttypes.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <string.h>
 
 #include <generated/autoconf.h>
@@ -36,7 +36,7 @@
 #endif
 
 typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
-typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t) sword_t;
+typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;
 #define FMT_WORD MUXDEF(CONFIG_ISA64, "0x%016" PRIx64, "0x%08" PRIx32)
 
 typedef word_t vaddr_t;
@@ -44,92 +44,6 @@ typedef MUXDEF(PMEM64, uint64_t, uint32_t) paddr_t;
 #define FMT_PADDR MUXDEF(PMEM64, "0x%016" PRIx64, "0x%08" PRIx32)
 typedef uint16_t ioaddr_t;
 
-// Original utils.h sections moved here to avoid circular include
-
-// ----------- state -----------
-
-enum { NEMU_RUNNING, NEMU_STOP, NEMU_END, NEMU_ABORT, NEMU_QUIT };
-
-typedef struct {
-  int state;
-  vaddr_t halt_pc;
-  uint32_t halt_ret;
-} NEMUState;
-
-extern NEMUState nemu_state;
-
-// ----------- timer -----------
-
-uint64_t get_time();
-
-// ----------- log -----------
-
-#define ANSI_FG_BLACK "\33[1;30m"
-#define ANSI_FG_RED "\33[1;31m"
-#define ANSI_FG_GREEN "\33[1;32m"
-#define ANSI_FG_YELLOW "\33[1;33m"
-#define ANSI_FG_BLUE "\33[1;34m"
-#define ANSI_FG_MAGENTA "\33[1;35m"
-#define ANSI_FG_CYAN "\33[1;36m"
-#define ANSI_FG_WHITE "\33[1;37m"
-#define ANSI_BG_BLACK "\33[1;40m"
-#define ANSI_BG_RED "\33[1;41m"
-#define ANSI_BG_GREEN "\33[1;42m"
-#define ANSI_BG_YELLOW "\33[1;43m"
-#define ANSI_BG_BLUE "\33[1;44m"
-#define ANSI_BG_MAGENTA "\33[1;45m"
-#define ANSI_BG_CYAN "\33[1;46m"
-#define ANSI_BG_WHITE "\33[1;47m"
-#define ANSI_NONE "\33[0m"
-
-#define ANSI_FMT(str, fmt) fmt str ANSI_NONE
-
-#define log_write(...)                                                         \
-  IFDEF(                                                                       \
-      CONFIG_TARGET_NATIVE_ELF, do {                                           \
-        extern FILE *log_fp;                                                   \
-        extern bool log_enable();                                              \
-        if (log_enable() && log_fp != NULL) {                                  \
-          fprintf(log_fp, __VA_ARGS__);                                        \
-          fflush(log_fp);                                                      \
-        }                                                                      \
-      } while (0))
-
-#define _Log(...)                                                              \
-  do {                                                                         \
-    printf(__VA_ARGS__);                                                       \
-    log_write(__VA_ARGS__);                                                    \
-  } while (0)
-
-// End of original utils.h sections
-
-// Original debug.h sections moved here to avoid circular include
-
-#include <stdio.h>
-
-#define Log(format, ...)                                                       \
-  _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", __FILE__, __LINE__,  \
-       __func__, ##__VA_ARGS__)
-
-#define Assert(cond, format, ...)                                              \
-  do {                                                                         \
-    if (!(cond)) {                                                             \
-      MUXDEF(                                                                  \
-          CONFIG_TARGET_AM,                                                    \
-          printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ##__VA_ARGS__),           \
-          (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", \
-                                   ##__VA_ARGS__)));                           \
-      IFNDEF(CONFIG_TARGET_AM, extern FILE * log_fp; fflush(log_fp));          \
-      extern void assert_fail_msg();                                           \
-      assert_fail_msg();                                                       \
-      assert(cond);                                                            \
-    }                                                                          \
-  } while (0)
-
-#define panic(format, ...) Assert(0, format, ##__VA_ARGS__)
-
-#define TODO() panic("please implement me")
-
-// End of original debug.h sections
+#include <debug.h>
 
 #endif
