e51c39218a3236899c38cd9c2b8451cc22bb1a3f
Zure <2819058133@qq.com>
2025-12-31 14:41:21 +0800

    fix: update COMMON_CFLAGS to include M extension for RISC-V architecture

--- abstract-machine/scripts/minirv-npc.mk
+++ abstract-machine/scripts/minirv-npc.mk
@@ -6,8 +6,8 @@ CC = minirv-gcc
 AS = minirv-gcc
 CXX = minirv-g++
 
-COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
-LDFLAGS       += -melf32lriscv                    # overwrite
+COMMON_CFLAGS += -march=rv32em_zicsr -mabi=ilp32e  # overwrite with M extension
+LDFLAGS       += -melf32lriscv                     # overwrite
 
 AM_SRCS += riscv/npc/libgcc/div.S \
            riscv/npc/libgcc/muldi3.S \
